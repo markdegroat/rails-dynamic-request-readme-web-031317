c491b94a3beb6e61f5bdd0072517829155e71681
markdegroat <mhdegroat@gmail.com>
2017-04-10 16:02:18 -0400

    Final

--- config/routes.rb
+++ config/routes.rb
@@ -53,4 +53,10 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
+  #get 'posts/:id', to: 'posts#show'
+  #becomes
+  resources :posts, only: :show
+
+
 end
